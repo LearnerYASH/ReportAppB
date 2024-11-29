f16fb9278de9241a22bd51ad65be08f511686b77
LearnerYASH <goelyash879147@gmail.com>
2024-11-29 23:17:38 +0530

    vvg

--- routes/auth.js
+++ routes/auth.js
@@ -95,3 +95,4 @@ router.get('/get-product-key', async (req, res) => {
         res.status(500).json({ success: false, message: 'Server error', error: error.message });
     }
 });
+module.exports = router;
